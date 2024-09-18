56ec2161d83f19d7663595d6aaf5b2d8a121544f
Bethelhem Misgina <bettymisg6@gmail.com>
2024-09-18 12:49:32 +0300

    api key removed

--- lib/core/utils/constant/constants.dart
+++ lib/core/utils/constant/constants.dart
@@ -3,9 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String googleApikey = Platform.isIOS
-      ? "AIzaSyAV8ljrYLYeaRH5XlnxVIJqDu5Y1EuLOJA"
-      : "AIzaSyDxXUL8bFNWPYQdnJVY16tNZuo00usZnwc";
+ 
 
   static BuildContext? globalContext;
 }
